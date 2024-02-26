362c81305b4cf92b031b7a24c5b0461ae0a6e053
Nikki Nikolenko <nikkinikolenko@MacBook-Pro-Nikki.local>
2024-02-26 14:26:16 -0700

    join button

--- packages/nextjs/app/sprintplanner/page.tsx
+++ packages/nextjs/app/sprintplanner/page.tsx
@@ -2,8 +2,21 @@ import type { NextPage } from "next";
 
 const SprintPlanner: NextPage = () => {
     return (
-      <div>hi</div>
-    );
-  };
+      <div className={`grid grid-cols-1 lg:grid-cols-6 px-6 lg:px-10 lg:gap-12 w-full max-w-7xl my-0`}>
+      <div className="col-span-5 grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10">
+        <div className="col-span-1 lg:col-span-2 flex flex-col gap-6">
+          <div className="z-10">
+            <div className="bg-base-100 rounded-3xl shadow-md shadow-secondary border border-base-300 flex flex-col mt-10 relative">
+              <button className="btn btn-secondary btn-sm">
+              {false && <span className="loading loading-spinner loading-xs"></span>}
+                Join group
+            </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )};
+
   
   export default SprintPlanner;
